87d6273b03d33fb7bb3ac2716be0b823637c7601
XandyGomes <x3.computadores@hotmail.com>
2024-09-11 22:23:24 -0300

    Aula.11.09.2024

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,20 +1,55 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Icon from 'react-native-vector-icons/MaterialIcons';
-import { Container, Form, Input, SubmitButton } from './styles';
+import {Container, Form, Input, SubmitButton} from './styles';
 
-export default function Main() {
+import api from '../services/api';
+
+export default class Main extends Component {
+  state = {
+    newUser: '',
+    users: [],
+  };
+
+  handleAddUser = async () => {
+    
+    const {users, newUsers } = this.state;
+
+    const response = await api.get(`/users/${newUser}`);
+
+    const data = {
+        name: response.data.name,
+        login: response.data.login,
+        bio: response.data.bio,
+        avatar: response.data.avatar_url,
+    }
+
+    this.setState({
+        users: [...users, data],
+        newUser: '',
+    })
+
+  };
+
+  render() {
+    const {users, newUser} = this.state;
+
     return (
-        <Container>
-            <Form>
-                <Input
-                    autoCorrect={false}
-                    autoCapitalize="none"
-                    placeholder="Adicionar usuário"
-                />
-                <SubmitButton>
-                    <Icon name="add" size={22} color="#fff" />
-                </SubmitButton>
-            </Form>
-        </Container>
+      <Container>
+        <Form>
+          <Input
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Adicionar usuário"
+            value={newUser}
+            onChangeText={text => this.setState({newUser: text})}
+            returnKeyType="send"
+            onSubmitEditing={this.handleAddUser}
+          />
+          <SubmitButton onPress={this.handleAddUser}>
+            <Icon name="add" size={22} color="#fff" />
+          </SubmitButton>
+        </Form>
+      </Container>
     );
+  }
 }
