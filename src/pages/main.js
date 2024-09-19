99fc732cb473502405c5c29e61ba866be570cef0
XandyGomes <x3.computadores@hotmail.com>
2024-09-18 22:14:30 -0300

    Aula.18.09

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
-import {Keyboard} from 'react-native';
+import {Keyboard, ActivityIndicator} from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import {
   Container,
@@ -21,32 +22,63 @@ export default class Main extends Component {
   state = {
     newUser: '',
     users: [],
+    loading: false,
   };
 
+  async componentDidMount() {
+    const users = await AsyncStorage.getItem('users');
+    if (users) {
+      this.setState({users: JSON.parse(users)});
+    }
+  }
+
+  async componentDidUpdate(_, prevState) {
+    const {users} = this.state;
+
+    if (prevState.users !== users) {
+      await AsyncStorage.setItem('users', JSON.stringify(users));
+    }
+  }
+
   handleAddUser = async () => {
-    const {users, newUser} = this.state;
+    try {
+      const {users, newUser} = this.state;
+      this.setState({loading: true});
 
-    const response = await api.get(`/users/${newUser}`);
+      const response = await api.get(`/users/${newUser}`);
 
-    const data = {
-      name: response.data.name,
-      login: response.data.login,
-      bio: response.data.bio,
-      avatar: response.data.avatar_url,
-    };
+      if (users.find(user => user.login === response.data.login)) {
+        alert('Usuário já adicionado!');
+        this.setState({
+          loading: false,
+        });
+        return;
+      }
 
-    this.setState({
-      users: [...users, data],
-      newUser: '',
-    });
+      const data = {
+        name: response.data.name,
+        login: response.data.login,
+        bio: response.data.bio,
+        avatar: response.data.avatar_url,
+      };
 
-    Keyboard.dismiss();
+      this.setState({
+        users: [...users, data],
+        newUser: '',
+        loading: false,
+      });
 
-    console.log(response.data);
+      Keyboard.dismiss();
+    } catch (error) {
+      alert('Usuário não encontrado!');
+      this.setState({
+        loading: false,
+      });
+    }
   };
 
   render() {
-    const {users, newUser} = this.state;
+    const {users, newUser, loading} = this.state;
 
     return (
       <Container>
@@ -60,15 +92,19 @@ export default class Main extends Component {
             returnKeyType="send"
             onSubmitEditing={this.handleAddUser}
           />
-          <SubmitButton onPress={this.handleAddUser}>
-            <Icon name="add" size={22} color="#fff" />
+          <SubmitButton loading={loading} onPress={this.handleAddUser}>
+            {loading ? (
+              <ActivityIndicator color="#fff" />
+            ) : (
+              <Icon name="add" size={22} color="#fff" />
+            )}
           </SubmitButton>
         </Form>
         <List
           showsVerticalScrollIndicator={false}
           data={users}
           keyExtractor={user => user.login}
-          renderItem={({ item }) => (
+          renderItem={({item}) => (
             <User>
               <Avatar source={{uri: item.avatar}} />
               <Name>{item.name}</Name>
